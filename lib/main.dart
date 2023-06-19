d41e280372258daf006f7946431c39429d099b47
unknown <returajsumon0808@gmail.com>
2023-06-19 23:35:17 +0600

    live 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,123 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopping List',
+      title: 'Counter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: CounterApp(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  final List<String> shoppingItems = [
-    'Apples',
-    'Bananas',
-    'Bread',
-    'Milk',
-    'Eggs',
-  ];
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+      if (counter == 5) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Counter value is 5!'),
+              actions: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else if (counter == 10) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SecondScreen()),
+        );
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      counter--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Shopping List"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Cart is empty")),
-              );
-            },
-          ),
-        ],
+        title: Text('Counter App'),
       ),
-      body: ListView.builder(
-        itemCount: shoppingItems.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Icon(Icons.shopping_bag),
-            title: Text(shoppingItems[index]),
-          );
-        },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Counter Value:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$counter',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: ElevatedButton(
+                    onPressed: _incrementCounter,
+                    child: Text('+'),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Flexible(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: _decrementCounter,
+                    child: Text('-'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Congratulations! You reached 10!',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
